c6a4578388320640ef057da9834b06e2ca20ab63
please <a0+1@berkeley.edu>
2024-06-29 04:16:11 -0700

    Enable formatting by Tab for extended input mode

--- src/app/editor.cljs
+++ src/app/editor.cljs
@@ -52,6 +52,10 @@
            #js [(cm-clj/syntax live-grammar/parser)
                 (.slice editor-extensions-to-load 1)]
            editor-extensions-to-load))
+       ;; enable formatting by Tab for extended input mode
+       (.of view/keymap (if (= extension-mode :extended)
+                          (.filter cm-clj/complete-keymap #(= "Tab" (aget % "key")))
+                          #js []))
        #_(.of view/keymap cm-clj/complete-keymap)
        #_(.of view/keymap historyKeymap)])))
 
